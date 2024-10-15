a32a3f8b643cd3816edb76b525082af459509a34
maxhilding <maxi.hilding@gmail.com>
2024-10-14 21:45:50 -0400

    task 3 done

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,19 +13,33 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+    res.send(JSON.stringify(books));
+  //return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let isbn = req.params.isbn;
+  let book_info = books[isbn];
+  res.send(JSON.stringify(book_info));
+  //return res.status(300).json({message: "Yet to be implemented"});
  });
-  
+  2
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let books_by_author = {};
+  let author = req.params.author;
+  let keys = Object.keys(books);
+  for (i=0; i<keys.length; i++) {
+    if (books[keys[i]].author == author) {
+        books_by_author[keys[i]] = books[keys[i]];
+    }
+  }
+  res.send(JSON.stringify(books_by_author));
+  //return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get all books based on title
